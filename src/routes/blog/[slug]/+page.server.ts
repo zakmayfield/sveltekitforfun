2fcc3209067bdc80314738b3857f6c8f1f5c733f
Zak Mayfield <zakmayfield@gmail.com>
2023-07-28 16:15:49 -0400

    error implementation

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,8 +1,12 @@
+import { error } from '@sveltejs/kit';
 import posts from '../data';
 import type { Post } from '../data';
 
 export function load({ params }): { post?: Post } {
 	const post = posts.find((post: Post) => post.slug === params.slug);
+
+	if (!post) throw error(404);
+
 	return {
 		post
 	};
