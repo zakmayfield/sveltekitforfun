4225e41ee1e0867f8268667333542d7ef41aac0e
Zak Mayfield <zakmayfield@gmail.com>
2023-08-01 09:39:36 -0400

    understanding how data is passed around server components and svelte components

---
+++ src/routes/blog/[slug]/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let data;
+</script>
+
+<div class="layout">
+	<main>
+		<slot />
+	</main>
+
+	<aside>
+		<h2>More Posts</h2>
+
+		<ul>
+			{#each data.summaries as { slug, title }}
+				<li><a href={`/blog/${slug}`}>{title}</a></li>
+			{/each}
+		</ul>
+	</aside>
+</div>
