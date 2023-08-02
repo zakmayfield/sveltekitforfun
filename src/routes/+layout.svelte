daecf558e174d3afbdb5d5fed9873a280caab7cb
Zak Mayfield <zakmayfield@gmail.com>
2023-08-02 15:15:52 -0400

    $lib/message for example with nested route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 		<li><a href="/">Home</a></li>
 		<li><a href="/about">About</a></li>
 		<li><a href="/blog">Blog</a></li>
+		<li><a href="/a/deeply/nested/route">A deeply nested route</a></li>
 	</ul>
 </nav>
 
---
+++ src/routes/a/deeply/nested/route/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { message } from '$lib/message';
+</script>
+
+<div>
+	<h1>Hello from the a deeply nested route:</h1>
+
+	<h2>Library message:</h2>
+	<p>{message}</p>
+</div>
